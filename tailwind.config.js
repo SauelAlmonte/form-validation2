8f38effa89a8b1a13e969442c833d30bf5989cf7
Salmonte <almontesauel@gmail.com>
2022-07-28 20:52:38 -0400

    Created needed files & directory

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],
+  content: ["./layouts/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
