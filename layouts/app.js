2aeeb06401a239f032e95af06024e55c84a5b881
Salmonte <almontesauel@gmail.com>
2022-07-30 22:54:11 -0400

    JavaScript/Interactivity

---
+++ layouts/app.js
@@ -0,0 +1,66 @@
+// document.getElementById("my-form")
+// console.log("username: ")
+
+// Example
+
+// const myUsername = "username"
+// console.log(myUsername)
+
+// Use
+
+// username
+// Step 1
+const username = document.getElementById("username")
+
+const usernameValue = username.value
+// Step 2
+console.log(usernameValue)
+
+// Step 3 If empty
+// A conditional step is required
+// A conditional step is required
+// example
+// if(what to check){
+//     what should happen
+// }
+// username.value
+
+// Email
+const email = document.getElementById("email")
+console.log(email)
+
+//Password
+const password = document.getElementById("password")
+console.log(password)
+
+// Password Confirm
+const password2 = document.getElementById("password-2")
+console.log(password2)
+
+// Steps
+// 1. Grab the DOM section
+// 2. Check if the value is empty
+// 3. If empty, give user some feedback
+// 4. Other, it's ok.
+
+// Different Method
+
+// Variable called "username"
+// const username = document.getElementById("username")
+
+// console.log(document.getElementById("username"))
+// Same
+// console.log(
+//     document.getElementById("username")
+// )
+
+//Email
+// console.log("email: ")
+// console.log(document.getElementById("email"))
+//
+// //Password
+// console.log("password: ")
+// console.log(document.getElementById("password"))
+//
+// console.log("confirm password: ")
+// console.log(document.getElementById("password-2"))
