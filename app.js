c4c90ee1f75d626c2ec66207442a53e0ef9bc090
Salmonte <almontesauel@gmail.com>
2022-09-01 20:54:39 -0400

    Making form interactive

--- app.js
+++ app.js
@@ -21,60 +21,74 @@ form.addEventListener( 'click', function (e){
     e.preventDefault()
     validateEmpty(username)
     validateEmpty(email)
+    validateEmpty(password)
+    validateEmpty(password2)
+    validatePassMatch(password,password2)
     validateIsEmail(email)
-    validateEmpty(password, password2)
-    validatePassMatch(password, password2)
-    validateMinLength(user)
+    validateEmail(email)
+    validateMinLength (password, password2)
 
 })
 
-function validateEmpty(input){
-    console.log(input)
-    if (input.value === '') {
-        showError(input)
+function validateEmpty(input) {
+    console.log(input.value)
+    // declaration
+    const inputName = input.name
+    //for empty value
+    if (input.value ==='') {
+        //functions for error or success
+        showError(input,`${inputName} field is empty`)
     } else {
-        showSuccess(input)
+        showSuccess(input,`${inputName} is entered`)
     }
+
 }
 
 // Show a message with error
-function showError(input) {
-    console.log("Username is Empty")
-}
+function showError(input, msg){
+    // uses styling from tailwind.css file
+    input.nextElementSibling.innerHTML = `<small class="error">${msg}</small>`
 
-// function showError(input){
-//     input.addEventListener('blur', function(e) {
-//         e.preventDefault()
-//         console.log("Input Empty")
-//         if(input.value === '') {
-//             e.target.nextElementSibling.innerHTML = '<small class="error">✗ Input Empty</small>'
-//         }
-//     })
-// }
+}
 
 // Show a message with success
 
-function showSuccess(input) {
-    console.log('You are ready to submit...')
-}
-
-// function showSuccess(input){
-//     input.addEventListener('blur', function (e){
-//         console.log('You are ready to submit')
-//         if(input.value !== '') {
-//             e.target.nextElementSibling.innerHTML = '<small class="success"> ✔ complete</small>'
-//         }
-//     })
-// }
-
-function validateIsEmail(email) {
-    console.log('You are ready to submit...')
-}
-
-function validatePassMatch(password, password2) {
+function showSuccess(input,msg) {
+    input.nextElementSibling.innerHTML =`<small class="success">${msg}</small>`
 
 }
 
-function validateMinLength (user) {
+function validateIsEmail (email) {
+    if (validateIsEmail(email)===true) { // return true
+        showSuccess(email,'valid')
+    } else {
+        showError(email, 'Email is invalid')
+    }
+}
 
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let result = re.test(String(email).toLowerCase())
+    console.log(result)
+    return result
+
+}
+
+function validatePassMatch (password, password2) {
+    if (password.value===password2.value && password.value!==''){
+        showSuccess(password2,`${password.name} matches ${password2.name}`)
+    }else {
+        showError(password2,'Error, not a match! Please re-enter the password')
+    }
+}
+
+function validateMinLength (input) {
+    console.log(input.value.length)
+    const inputName = input.name
+
+    if (input.value.length < 4 && input.value===password.value){
+        showError(input, `${inputName} is too short`)
+    } else {
+        showSuccess(input,`${inputName} is long enough`)
+    }
 }
